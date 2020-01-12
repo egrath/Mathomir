797a65c68f5eaa80eb66d9bae7537b068c9504fc
Egon Rath <egon.rath@gmail.com>
2020-01-12 18:48:39 +0100

    Initial upload

---
+++ Mathomir/stdafx.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+#ifndef WINVER				
+#define WINVER 0x0601       // Windows 7 Minimum
+#endif
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	
+
+#define _AFX_ALL_WARNINGS
+
+#include <afxwin.h>
+#include <afxext.h>
+#include <afxdisp.h>
+#include <afxcmn.h>			
+
